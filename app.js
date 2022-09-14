10c9cf910c384b7db327e4f01e5b6f2418daffd0
Cody <Cojo341@gmail.com>
2022-09-14 16:19:26 -0400

    Finished TODO's

--- app.js
+++ app.js
@@ -4,7 +4,29 @@ console.log(
 );
 
 // Exercise 1
-
+let firstName;
+let lastName;
+var age;
 // Exercise 2
-
+firstName = "Cody";
+lastName = "V";
+age = 26;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+//Exercise 4
+let price = 19.99;
+let isOnSale = true;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`"The ${selectedSize} size shirt you picked is ${inStock ? "in stock" : "not in stock"} and is \$${19.99}. This item ${isOnSale ? `is on sale for ${salePercentage}\% off.`: "is not on sale." }"`)
